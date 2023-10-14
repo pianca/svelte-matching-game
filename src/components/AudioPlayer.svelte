90a3815b0c6a364660d9d6f17e80c9553d35f6ee
mauro pianca <pianca.mauro@gmail.com>
2023-10-14 17:36:33 +0200

    used an external lib to play the sounds....

---
+++ src/components/AudioPlayer.svelte
@@ -0,0 +1,30 @@
+<!-- eslint-disable -->
+<script context="module" lang="ts">
+  let player: HTMLAudioElement;
+  export function stop() {
+    player.pause();
+  }
+  export function play() {
+    player.play();
+  }
+</script>
+
+<script lang="ts">
+  import { onMount } from "svelte";
+  // import { writable, get } from 'svelte/store';
+
+  export let src: string;
+  //export let id: string;
+
+  let audio: HTMLAudioElement;
+
+  onMount(() => {
+    console.log("on mount audio");
+    audio = new Audio(src);
+    player = audio;
+  });
+</script>
+
+<div>
+  <audio bind:this={audio} />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,253 +1,255 @@
 <script lang="ts">
-	import { emoji } from './emoji'
+  import { Sound } from "svelte-sound";
+  import { emoji } from "./emoji";
 
-	type State = 'start' | 'playing' | 'paused' | 'won' | 'lost'
+  type State = "start" | "playing" | "paused" | "won" | "lost";
 
-	let state: State = 'start'
-	let size = 20;
-	let grid = createGrid()
-	let maxMatches = grid.length / 2
-	let selected: number[] = []
-	let matches: string[] = []
-	let timerId: number | null = null
-	let time = 300
+  let win = "/person-clapping-sound-effect.mp3";
+  let fart = "/fart-sound-effect.mp3";
+  let lose = "/losing-sound-effect.mp3";
+  let nice = "/short-winning-brass-sound-effect.mp3";
 
-	function createGrid() {
-		// only want unique cards
-		let cards = new Set<string>()
-		// half because we duplicate the cards
-		let maxSize = size / 2
+  let state: State = "start";
+  let size = 20;
+  let grid = createGrid();
+  let maxMatches = grid.length / 2;
+  let selected: number[] = [];
+  let matches: string[] = [];
+  let timerId: number | null = null;
+  let time = 300;
 
-		while (cards.size < maxSize) {
-			// pick random emoji
-			const randomIndex = Math.floor(Math.random() * emoji.length)
-			cards.add(emoji[randomIndex])
-		}
+  function createGrid() {
+    // only want unique cards
+    let cards = new Set<string>();
+    // half because we duplicate the cards
+    let maxSize = size / 2;
 
-		// duplicate and shuffle cards
-		return shuffle([...cards, ...cards])
-	}
-
-	function shuffle<Items>(array: Items[]) {
-		return array.sort(() => Math.random() - 0.5)
-	}
-
-	function startGameTimer() {
-		function countdown() {
-			state !== 'paused' && (time -= 1)
-		}
-		timerId = setInterval(countdown, 1000)
-	}
-
-	function selectCard(cardIndex: number) {
-		selected = selected.concat(cardIndex)
-	}
-
-	function matchCards() {
-		// array destructuring can have any name for the values
-		const [first, second] = selected
-
-		if (grid[first] === grid[second]) {
-			matches = matches.concat(grid[first])
-		}
-
-		// clear selected
-		setTimeout(() => (selected = []), 2500)
-	}
-
-	function pauseGame(e: KeyboardEvent) {
-		if (e.key === 'Escape') {
-			switch (state) {
-				case 'playing':
-					state = 'paused'
-					break
-				case 'paused':
-					state = 'playing'
-					break
-			}
-		}
-	}
-
-	function resetGame() {
-		timerId && clearInterval(timerId)
-		grid = createGrid()
-		maxMatches = grid.length / 2
-		selected = []
-		matches = []
-		timerId = null
-		time = 300
-	}
-
-	function gameWon() {
-		state = 'won'
-		resetGame()
-	}
-
-	function gameLost() {
-		state = 'lost'
-		resetGame()
-	}
-
-	$: if (state === 'playing') {
-		//	in case you pause the game
-		!timerId && startGameTimer()
-	}
-
-	$: selected.length === 2 && matchCards()
-	$: maxMatches === matches.length && gameWon()
-	$: time === 0 && gameLost()
-
-	let audio:HTMLAudioElement;
-  let isPlaying = false;
-  
-	let mp3Url = "/person-clapping-sound-effect.mp3";
-
-  function togglePlay() {
-    if (isPlaying) {
-      audio.pause();
-    } else {
-      audio.play();
+    while (cards.size < maxSize) {
+      // pick random emoji
+      const randomIndex = Math.floor(Math.random() * emoji.length);
+      cards.add(emoji[randomIndex]);
     }
-    isPlaying = !isPlaying;
+
+    // duplicate and shuffle cards
+    return shuffle([...cards, ...cards]);
   }
+
+  function shuffle<Items>(array: Items[]) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  function startGameTimer() {
+    function countdown() {
+      state !== "paused" && (time -= 1);
+    }
+    timerId = setInterval(countdown, 1000);
+  }
+
+  function selectCard(cardIndex: number) {
+    selected = selected.concat(cardIndex);
+  }
+
+  function matchCards() {
+    // array destructuring can have any name for the values
+    const [first, second] = selected;
+
+    if (grid[first] === grid[second]) {
+      matches = matches.concat(grid[first]);
+      niceSound.play();
+    } else {
+      fartSound.play();
+    }
+
+    // clear selected
+    setTimeout(() => (selected = []), 2500);
+  }
+
+  function pauseGame(e: KeyboardEvent) {
+    if (e.key === "Escape") {
+      switch (state) {
+        case "playing":
+          state = "paused";
+          break;
+        case "paused":
+          state = "playing";
+          break;
+      }
+    }
+  }
+
+  function resetGame() {
+    timerId && clearInterval(timerId);
+    grid = createGrid();
+    maxMatches = grid.length / 2;
+    selected = [];
+    matches = [];
+    timerId = null;
+    time = 300;
+  }
+
+  function gameWon() {
+    winSound.play();
+    state = "won";
+    resetGame();
+  }
+
+  function gameLost() {
+    loseSound.play();
+    state = "lost";
+    resetGame();
+  }
+
+  $: if (state === "playing") {
+    //	in case you pause the game
+    !timerId && startGameTimer();
+  }
+
+  $: selected.length === 2 && matchCards();
+  $: maxMatches === matches.length && gameWon();
+  $: time === 0 && gameLost();
+
+  const winSound = new Sound(win);
+  const fartSound = new Sound(fart);
+  const loseSound = new Sound(lose);
+  const niceSound = new Sound(nice);
 </script>
 
 <svelte:window on:keydown={pauseGame} />
 
-<div>
-	<h2>MP3 Player</h2>
-	<audio bind:this={audio} src={mp3Url} preload="auto"></audio>
-	<button on:click={togglePlay}>
-	  {isPlaying ? 'Pause' : 'Play'}
-	</button>
+{#if state === "start"}
+  <h1>Matching game</h1>
+  <button on:click={() => (state = "playing")}>Play</button>
+{/if}
+
+{#if state === "paused"}
+  <h1>Game paused</h1>
+{/if}
+
+{#if state === "playing"}
+  <h1 class="timer" class:pulse={time <= 10}>
+    {time}
+  </h1>
+
+  <div class="matches">
+    {#each matches as card}
+      <div>{card}</div>
+    {/each}
   </div>
 
-{#if state === 'start'}
-	<h1>Matching game</h1>
-	<button on:click={() => (state = 'playing')}>Play</button>
+  <div class="cards">
+    {#each grid as card, cardIndex}
+      {@const isSelected = selected.includes(cardIndex)}
+      {@const isSelectedOrMatch =
+        selected.includes(cardIndex) || matches.includes(card)}
+      {@const match = matches.includes(card)}
+      {@const isSelectedOrMatchOrMaxSelectionReached =
+        selected.length == 2 ||
+        selected.includes(cardIndex) ||
+        matches.includes(card)}
+
+      <button
+        class="card"
+        class:selected={isSelected}
+        class:flip={isSelectedOrMatch}
+        disabled={isSelectedOrMatchOrMaxSelectionReached}
+        on:click={() => selectCard(cardIndex)}
+      >
+        <div class="back" class:match>
+          {card}
+        </div>
+      </button>
+    {/each}
+  </div>
 {/if}
 
-{#if state === 'paused'}
-	<h1>Game paused</h1>
+{#if state === "lost"}
+  <h1>You lost! 💩</h1>
+  <button on:click={() => (state = "playing")}>Play again</button>
 {/if}
 
-{#if state === 'playing'}
-	<h1 class="timer" class:pulse={time <= 10}>
-		{time}
-	</h1>
-
-	<div class="matches">
-		{#each matches as card}
-			<div>{card}</div>
-		{/each}
-	</div>
-
-	<div class="cards">
-		{#each grid as card, cardIndex}
-			{@const isSelected = selected.includes(cardIndex)}
-			{@const isSelectedOrMatch =
-				selected.includes(cardIndex) || matches.includes(card)}
-			{@const match = matches.includes(card)}
-			{@const isSelectedOrMatchOrMaxSelectionReached = 
-				selected.length == 2 ||	selected.includes(cardIndex) || matches.includes(card)}
-
-			<button
-				class="card"
-				class:selected={isSelected}
-				class:flip={isSelectedOrMatch}
-				disabled={isSelectedOrMatchOrMaxSelectionReached}
-				on:click={() => selectCard(cardIndex)}
-			>
-				<div class="back" class:match>
-					{card}
-				</div>
-			</button>
-		{/each}
-	</div>
-{/if}
-
-{#if state === 'lost'}
-	<h1>You lost! 💩</h1>
-	<button on:click={() => (state = 'playing')}>Play again</button>
-{/if}
-
-{#if state === 'won'}
-	<h1>You win! 🎉</h1>
-	<button on:click={() => (state = 'playing')}>Play again</button>
+{#if state === "won"}
+  <h1>You win! 🎉</h1>
+  <button on:click={() => (state = "playing")}>Play again</button>
 {/if}
 
 <style>
-	.cards {
-		display: grid;
-		grid-template-columns: repeat(5, 1fr); 
-		gap: 0.4rem;
-	}
+  .cards {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 0.4rem;
+  }
 
-	.card {
-		height: 140px;
-		width: 140px;
-		font-size: 4rem;
-		background-image: repeating-linear-gradient(45deg, #d5d5dd 0, #d5d5dd 1.8px, transparent 0, transparent 50%);
-		background-color: #47c8ff;
-		background-size: 8px 8px;
-		/* background-color: var(--bg-2); */
-		transition: rotate 0.8s ease-out;
-		transform-style: preserve-3d;
+  .card {
+    height: 140px;
+    width: 140px;
+    font-size: 4rem;
+    background-image: repeating-linear-gradient(
+      45deg,
+      #d5d5dd 0,
+      #d5d5dd 1.8px,
+      transparent 0,
+      transparent 50%
+    );
+    background-color: #47c8ff;
+    background-size: 8px 8px;
+    /* background-color: var(--bg-2); */
+    transition: rotate 0.8s ease-out;
+    transform-style: preserve-3d;
 
-		&.selected {
-			border: 4px solid var(--border);
-		}
+    &.selected {
+      border: 4px solid var(--border);
+    }
 
-		&.flip {	
-			background-image: none;
-			background-color: #47c8ff;
-			rotate: y 180deg;
-			pointer-events: none;
-		}
+    &.flip {
+      background-image: none;
+      background-color: #47c8ff;
+      rotate: y 180deg;
+      pointer-events: none;
+    }
 
-		& .back {
-			background-image: none;
-			background-color: #334455;
+    & .back {
+      background-image: none;
+      background-color: #334455;
 
-			position: absolute;
-			inset: 0;
-			display: grid;
-			place-content: center;
-			backface-visibility: hidden;
-			rotate: y 180deg;
-		}
-		
-		& .match {
-			transition: opacity 0.3s ease-out;
-			background-image: none;
-			background-color: #33FF33;
-			opacity: 0.4;
-		}
-	}
+      position: absolute;
+      inset: 0;
+      display: grid;
+      place-content: center;
+      backface-visibility: hidden;
+      rotate: y 180deg;
+    }
 
-	.card:hover {
-		background-color: #ff9a4776;
-	}
+    & .match {
+      transition: opacity 0.3s ease-out;
+      background-image: none;
+      background-color: #33ff33;
+      opacity: 0.4;
+    }
+  }
 
-	.matches {
-		display: flex;
-		gap: 1rem;
-		margin-block: 2rem;
-		font-size: 3rem;
-	}
+  .card:hover {
+    background-color: #ff9a4776;
+  }
 
-	.timer {
-		transition: color 0.3s ease;
-	}
+  .matches {
+    display: flex;
+    gap: 1rem;
+    margin-block: 2rem;
+    font-size: 3rem;
+  }
 
-	.pulse {
-		color: var(--pulse);
-		animation: pulse 1s infinite ease;
-	}
+  .timer {
+    transition: color 0.3s ease;
+  }
 
-	@keyframes pulse {
-		to {
-			scale: 1.4;
-		}
-	}
+  .pulse {
+    color: var(--pulse);
+    animation: pulse 1s infinite ease;
+  }
+
+  @keyframes pulse {
+    to {
+      scale: 1.4;
+    }
+  }
 </style>
